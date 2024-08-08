437e766e50339f3dbea49b6d76d679775a81153f
Kevin <kevhoang97@gmail.com>
2024-08-07 23:44:48 -0400

    added models

---
+++ models/reaction.js
@@ -0,0 +1,25 @@
+const reactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId(),
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        max_length: 280,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+    {
+      _id: false, // Disable the creation of _id field for nested documents
+    }
+  );
+  
---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const validator = require("validator");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max_length: 50,
+      validate: {
+        validator: validator.isEmail,
+        message: "Not a valid email address",
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
